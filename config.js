6d4c91d430eb29c701f1163255b255bb50f57e9c
Muhammad Nurcahyo Pratomo <6202071+cohayfun@users.noreply.github.com>
2021-02-15 22:50:44 +0700

    Make config.js
    file config.js  for configuration global common

---
+++ config.js
@@ -0,0 +1,34 @@
+/*
+ * Disable developer tool like (elements, console, source, application, network, etc) and deadlock injection browser
+ * Copyright (c) 2021 Muhammad Nurcahyo Pratomo
+ *
+ * Licensed under the MIT
+ * 	http://www.opensource.org/licenses/mit-license.php
+ *
+ */
+
+/*
+	Note configuration
+	
+	@activeMode
+	activeMode is for active DDoS protected
+	true is active
+	false is non active
+	
+	@ipShow
+	ipShow is for show IP client
+	true is show
+	false is non show
+	
+	@dnsShow
+	dnsShow is for show IP client
+	true is show
+	false is non show
+*/
+
+/* Configuration variable */
+var activeMode			= true;
+var ipShow				= true;
+var dnsShow				= true;
+const backgroundColor	= '#fff';
+const copyright			= 'Your name company';
