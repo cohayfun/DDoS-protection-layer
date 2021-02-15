3186bb477f20d970d029079252474eebbc53f7ce
Muhammad Nurcahyo Pratomo <6202071+cohayfun@users.noreply.github.com>
2021-02-15 22:55:07 +0700

    Create configuration.js
    Make sure name

---
+++ configuration.js
@@ -0,0 +1,29 @@
+/*
+ * Configuration global common
+ * Copyright (c) 2021 Muhammad Nurcahyo Pratomo
+ *
+ * Licensed under the MIT
+ * 	http://www.opensource.org/licenses/mit-license.php
+ *
+ *	@activeMode
+ *	activeMode is for active DDoS protected
+ *	true is active
+ *	false is non active
+ *	
+ *	@ipShow
+ *	ipShow is for show IP client
+ *	true is show
+ *	false is non show
+ *	
+ *	@dnsShow
+ *	dnsShow is for show IP client
+ *	true is show
+ *	false is non show
+ */
+
+/* Configuration variable */
+var activeMode			= true;
+var ipShow				= true;
+var dnsShow				= true;
+const backgroundColor	= '#fff';
+const copyright			= 'Your name company';
